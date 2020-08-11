8b1248757e3966e9423363cfa98a97b5d0cfa407
efarioli <efarioli@gmail.com>
2020-08-11 16:20:28 +0100

    adding the script file and defining the URL of the API to use

---
+++ script.js
@@ -0,0 +1,15 @@
+/**
+ * Asignarle un callback al evento click de todos los links del nav para que puedan ir a buscar con AJAX el archivo que le corresponde y poder mostrarlo dentro del <main>
+ * 
+ * 
+ * 
+ * PRIMER BONUS
+ * 
+ * Por cada vez que se le hace click a un link, tendria que poder cambiar la URL y detectar cambios en la URL. 
+ * 
+ * 
+ * SEGUNDO BONUS
+ * 
+ * Si el usuario vuelve para atras en el historial, deberia poder ver el contenido que le corresponde a la URL.
+ */
+const url = "https://jsonplaceholder.typicode.com"
