f66d1784bee5eeb28805c696892d40c113361ea8
efarioli <efarioli@gmail.com>
2020-08-17 12:32:14 +0100

    changing the style for the active link in the navigation bar

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 See live preview on:
 &nbsp;&nbsp;&nbsp;&nbsp;https://efarioli.github.io/vanillajs-spa-app-withrouter/index.html
-# Features:
-* The web-app uses hash-based URLs, for example, #home, #posts, etc
-* The web-app manager the browser history,  you can back to a webpage, and forward as well.
-* The router in the web application is not dependent on a server-side component.
-* The different URL between the URL are direct shareable by copy and paste.
-* If user access to index.html, the router redirects to #home, without adding the index.html to browser history.
-* The router keeps track of what route is active, so by creating css class we can differentiate that.
+
+## Features:
+
+- The web-app uses hash-based URLs, for example, #home, #posts, etc
+- The web-app manager the browser history, you can back to a webpage, and forward as well.
+- The router in the web application is not dependent on a server-side component.
+- The different URL between the URL are direct shareable by copy and paste.
+- If user access to index.html, the router redirects to #home, without adding the index.html to browser history.
+- The router keeps track of what route is active, so by creating css class we can differentiate that.
+
+  ![Alt Text](https://media.giphy.com/media/LQv5xR44rLPRrSNZYM/giphy.gif)
--- script.js
+++ script.js
@@ -1,64 +1,63 @@
 /**
  * Asignarle un callback al evento click de todos los links del nav para que puedan ir a buscar con AJAX el archivo que le corresponde y poder mostrarlo dentro del <main>
- * 
- * 
- * 
+ *
+ *
+ *
  * PRIMER BONUS
- * 
- * Por cada vez que se le hace click a un link, tendria que poder cambiar la URL y detectar cambios en la URL. 
- * 
- * 
+ *
+ * Por cada vez que se le hace click a un link, tendria que poder cambiar la URL y detectar cambios en la URL.
+ *
+ *
  * SEGUNDO BONUS
- * 
+ *
  * Si el usuario vuelve para atras en el historial, deberia poder ver el contenido que le corresponde a la URL.
  */
-const url = "https://jsonplaceholder.typicode.com"
+const url = 'https://jsonplaceholder.typicode.com'
 
-const prefixGithubPages = `vanillajs-spa-app-withrouter/` //the correct setup for githubpages
-// const prefixGithubPages = ``//local dev for othe any enviroment
-const routerView = document.querySelector("#router")
-const navAnchors = document.querySelectorAll(".link")
+// const prefixGithubPages = `vanillajs-spa-app-withrouter/` //the correct setup for githubpages
+const prefixGithubPages = `` //local dev for othe any enviroment
+const routerView = document.querySelector('#router')
+const navAnchors = document.querySelectorAll('.link')
 
-const ajax = (url, queryString, method, responseType = "json") => {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest
-        xhr.responseType = responseType
-        xhr.open(method, `${url}${queryString}`)
-        xhr.addEventListener("load", () => {
-            resolve(xhr.response)
-        })
-        xhr.send()
+const ajax = (url, queryString, method, responseType = 'json') => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest()
+    xhr.responseType = responseType
+    xhr.open(method, `${url}${queryString}`)
+    xhr.addEventListener('load', () => {
+      resolve(xhr.response)
     })
+    xhr.send()
+  })
 }
 
-const createUserArticleHTML = (user) => {
-    //create an HTML element (article) that represents an user
-    const article = document.createElement("article")
-    article.classList.add("card")
-    article.innerHTML =
-        `<img src="" alt=""><h1></h1><h2></h2>
+const createUserArticleHTML = user => {
+  //create an HTML element (article) that represents an user
+  const article = document.createElement('article')
+  article.classList.add('card')
+  article.innerHTML = `<img src="" alt=""><h1></h1><h2></h2>
    <a href="#">Ver Posteos de  </a>`
-    article.children[0].src = `./pic/${user.id}.jpg`
-    article.children[1].innerText = `${user.name}`
-    article.children[2].innerText = `${user.email}`
-    article.children[3].href = `/${prefixGithubPages}#posts?userId=${user.id}`
-    article.children[3].innerText = `Ver Posteos de ${user.name}`
-    return article;
+  article.children[0].src = `./pic/${user.id}.jpg`
+  article.children[1].innerText = `${user.name}`
+  article.children[2].innerText = `${user.email}`
+  article.children[3].href = `/${prefixGithubPages}#posts?userId=${user.id}`
+  article.children[3].innerText = `Ver Posteos de ${user.name}`
+  return article
 }
 
-const createUserH2HTML = (user) => {
-    //create an H2 element that represents an user - Username and thumbnail picture
-    const h2 = document.createElement("h2")
-    h2.innerHTML = `<span>Posts for user:</span><span><img src=""  alt=""/></span>`
-    console.log(h2.childNodes[1])
-    h2.children[0].innerText = `Posts for user: ${user.name}`
-    h2.children[1].children[0].src = `./pic/${user.id}.jpg`
-    return h2;
+const createUserH2HTML = user => {
+  //create an H2 element that represents an user - Username and thumbnail picture
+  const h2 = document.createElement('h2')
+  h2.innerHTML = `<span>Posts for user:</span><span><img src=""  alt=""/></span>`
+  console.log(h2.childNodes[1])
+  h2.children[0].innerText = `Posts for user: ${user.name}`
+  h2.children[1].children[0].src = `./pic/${user.id}.jpg`
+  return h2
 }
 
-const createPostPerUserHeader = (user) => {
-    const div = document.createElement("div")
-    div.innerHTML = `
+const createPostPerUserHeader = user => {
+  const div = document.createElement('div')
+  div.innerHTML = `
     <div class="container">
     <h2>Posts for user <span><img src="./pic/1.jpg"  alt=""/></span></h2>
     <ul class="responsive-table" id="rt">
@@ -69,12 +68,12 @@ const createPostPerUserHeader = (user) => {
   </li>
     </ul>
   </div>`
-    div.children[0].replaceChild(createUserH2HTML(user), div.children[0].children[0]);
-    return div
+  div.children[0].replaceChild(createUserH2HTML(user), div.children[0].children[0])
+  return div
 }
 const createViewAllPostHeader = () => {
-    const div = document.createElement("div")
-    div.innerHTML = `
+  const div = document.createElement('div')
+  div.innerHTML = `
     <div class="container">
     <h2>All Post</h2>
     <ul class="responsive-table" >
@@ -86,185 +85,182 @@ const createViewAllPostHeader = () => {
   </li>
     </ul>
   </div>`
-    return div
+  return div
 }
 
 const replaceInLi = (li, post) => {
-    li.children[0].innerText = post.id
-    li.children[1].innerText = post.title
-    li.children[2].innerText = post.body
-
+  li.children[0].innerText = post.id
+  li.children[1].innerText = post.title
+  li.children[2].innerText = post.body
 }
-const createLiPost = (post) => {
-    let li = document.createElement("li")
-    li.classList.add("table-row")
-    li.innerHTML =
-        `<div class="col col-1" data-label="Post Id"></div>
+const createLiPost = post => {
+  let li = document.createElement('li')
+  li.classList.add('table-row')
+  li.innerHTML = `<div class="col col-1" data-label="Post Id"></div>
         <div class="col col-2" data-label="Title"></div>
         <div class="col col-3" data-label="Post Body"></div>`
-    replaceInLi(li, post)
-    return li
+  replaceInLi(li, post)
+  return li
 }
 
-const createLiPostExt = (post) => {
-    let li = document.createElement("li")
-    li.classList.add("table-row")
-    li.innerHTML =
-        `<div class="col col-1v2" data-label="Post Id"></div>
+const createLiPostExt = post => {
+  let li = document.createElement('li')
+  li.classList.add('table-row')
+  li.innerHTML = `<div class="col col-1v2" data-label="Post Id"></div>
         <div class="col col-2v2" data-label="Title"></div>
         <div class="col col-3v2" data-label="Post Body"></div>
         <div class="col col-4v2" data-label="Post UserId"></div>`
-    replaceInLi(li, post)
-    li.children[3].innerText = post.userId
-    return li
+  replaceInLi(li, post)
+  li.children[3].innerText = post.userId
+  return li
 }
 
 async function doCreateUserView(routerView) {
-    try {
-        //get all users
-        const users = await ajax(url,`/users`, "get")
-        routerView.innerHTML = ""
+  try {
+    //get all users
+    const users = await ajax(url, `/users`, 'get')
+    routerView.innerHTML = ''
 
-        let articles = document.createDocumentFragment()
-        users.forEach(user => {
-            articles.appendChild(createUserArticleHTML(user))
-        })
-        routerView.appendChild(articles)
-
-    } catch (err) {
-        console.log(err)
-    }
+    let articles = document.createDocumentFragment()
+    users.forEach(user => {
+      articles.appendChild(createUserArticleHTML(user))
+    })
+    routerView.appendChild(articles)
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 async function doCreateViewPostsPerUser(routerView, userId) {
-    let div
-    let flag = false
-    try {
-        //get user data
-        const user = await ajax(url, `/users/${userId}`, "get")
-        //addressing the problem when the APi do not return any info because the user does not exist
-        if ((typeof user.id) === "undefined") {
-            routerView.innerHTML = "<h1>404 - Page Not Found</h1>"
-            flag = true
-            return
-        }
-        div = createPostPerUserHeader(user)
-
-    } catch (err) {
-        console.log(err + " Problem loading the The user information from Api")
+  let div
+  let flag = false
+  try {
+    //get user data
+    const user = await ajax(url, `/users/${userId}`, 'get')
+    //addressing the problem when the APi do not return any info because the user does not exist
+    if (typeof user.id === 'undefined') {
+      routerView.innerHTML = '<h1>404 - Page Not Found</h1>'
+      flag = true
+      return
     }
+    div = createPostPerUserHeader(user)
+  } catch (err) {
+    console.log(err + ' Problem loading the The user information from Api')
+  }
 
-    try {
-        //addressing the problem when the APi do not return any info because the user does not exist
-        if (flag) return
-        //get all the post for a particular user
-        const posts = await ajax(url,`/posts?userId=${userId}`, "get")
-        let fragment = document.createDocumentFragment()
-        posts.forEach(post => {
-            fragment.appendChild(createLiPost(post))
-
-        })
-        let ul = div.children[0].children[1]
-        ul.appendChild(fragment)
-        routerView.innerHTML = div.innerHTML
-
-    } catch (err) {
-        console.log(err + " Problem loading the The posts per User from Api")
-    }
+  try {
+    //addressing the problem when the APi do not return any info because the user does not exist
+    if (flag) return
+    //get all the post for a particular user
+    const posts = await ajax(url, `/posts?userId=${userId}`, 'get')
+    let fragment = document.createDocumentFragment()
+    posts.forEach(post => {
+      fragment.appendChild(createLiPost(post))
+    })
+    let ul = div.children[0].children[1]
+    ul.appendChild(fragment)
+    routerView.innerHTML = div.innerHTML
+  } catch (err) {
+    console.log(err + ' Problem loading the The posts per User from Api')
+  }
 }
 async function doCreateAllPostsView(routerView) {
-    let div
-    try {
+  let div
+  try {
+    const posts = await ajax(url, `/posts`, 'get')
 
-        const posts = await ajax(url,`/posts`, "get")
+    let fragment = document.createDocumentFragment()
+    posts.forEach(post => {
+      fragment.appendChild(createLiPostExt(post))
+    })
+    div = createViewAllPostHeader()
+    let ul = div.children[0].children[1]
+    ul.appendChild(fragment)
 
-        let fragment = document.createDocumentFragment()
-        posts.forEach(post => {
-            fragment.appendChild(createLiPostExt(post))
-        })
-        div = createViewAllPostHeader()
-        let ul = div.children[0].children[1]
-        ul.appendChild(fragment)
-
-        routerView.innerHTML = div.innerHTML
-
-    } catch (err) {
-        console.log(err + " Problem loading ALL The posts from Api")
-    }
+    routerView.innerHTML = div.innerHTML
+  } catch (err) {
+    console.log(err + ' Problem loading ALL The posts from Api')
+  }
 }
 
 const onRouteChanged = () => {
+  const hash = window.location.hash
 
-    const hash = window.location.hash;
+  if (!(routerView instanceof HTMLElement)) {
+    throw new ReferenceError('No router view element available for rendering')
+  }
 
-    if (!(routerView instanceof HTMLElement)) {
-        throw new ReferenceError("No router view element available for rendering");
+  switch (hash) {
+    case '#home':
+      routerView.innerHTML = '<h1>Home page</h1>'
+      break
+
+    case '#usuarios':
+      doCreateUserView(routerView)
+      break
+    case '#posts':
+      doCreateAllPostsView(routerView)
+      break
+    case String(hash.match(/#posts\?userId=\d{1,}/g)):
+      doCreateViewPostsPerUser(routerView, hash.split('userId=')[hash.split('userId=').length - 1])
+      break
+    case '#contacto':
+      routerView.innerHTML = '<h1>Contacto</h1>'
+      break
+
+    default:
+      routerView.innerHTML = '<h1>404 - Page Not Found</h1>'
+      break
+  }
+
+  let hashIdForActive = hash.split('?')[0]
+  navAnchors.forEach(anchor => {
+    if (anchor.classList.contains('active')) {
+      anchor.classList.remove('active')
     }
-
-    switch (hash) {
-        case "#home":
-            routerView.innerHTML = "<h1>Home page</h1>";
-            break
-
-        case "#usuarios":
-            doCreateUserView(routerView)
-            break
-        case "#posts":
-            doCreateAllPostsView(routerView)
-            break
-        case String(hash.match(/#posts\?userId=\d{1,}/g)):
-            doCreateViewPostsPerUser(routerView, hash.split('userId=')[hash.split('userId=').length - 1])
-            break
-        case "#contacto":
-            routerView.innerHTML = "<h1>Contacto</h1>";
-            break
-
-        default:
-            routerView.innerHTML = "<h1>404 - Page Not Found</h1>";
-            break;
+    if (`#${anchor.href.split('#')[anchor.href.split('#').length - 1]}` === hashIdForActive) {
+      anchor.classList.add('active')
     }
-
-    let hashIdForActive = hash.split("?")[0]
-    navAnchors.forEach(anchor => {
-        if (anchor.classList.contains("active")) { anchor.classList.remove("active") }
-        if (`#${anchor.href.split("#")[anchor.href.split("#").length - 1]}` === hashIdForActive) {
-            anchor.classList.add("active")
-        }
-    })
+  })
 }
 
-window.addEventListener("hashchange", onRouteChanged)
+window.addEventListener('hashchange', onRouteChanged)
 
+window.addEventListener('load', () => {
+  console.log('loadin....')
 
-window.addEventListener("load", () => {
-    console.log("loadin....")
+  if (window.location.href == `${window.location.origin}/${prefixGithubPages}index.html`) {
+    window.location.href = `${window.location.origin}/${prefixGithubPages}#home`
 
-    if (window.location.href == `${window.location.origin}/${prefixGithubPages}index.html`) {
-        window.location.href = `${window.location.origin}/${prefixGithubPages}#home`
-
-        history.pushState({
-            id: null
-        }, null, `${window.location.origin}/${prefixGithubPages}#home`);
-    }
-    onRouteChanged()
+    history.pushState(
+      {
+        id: null
+      },
+      null,
+      `${window.location.origin}/${prefixGithubPages}#home`
+    )
+  }
+  onRouteChanged()
 })
 
-
 navAnchors.forEach(anchor => {
-    //anchors or links
-    anchor.addEventListener("click", e => {
-        e.stopPropagation()
-        e.preventDefault()
-        let flag = window.location.href.toLowerCase().includes("index")
+  //anchors or links
+  anchor.addEventListener('click', e => {
+    e.stopPropagation()
+    e.preventDefault()
+    let flag = window.location.href.toLowerCase().includes('index')
 
-        if (flag) {
-            history.pushState({
-                id: null
-            }, null, `https://${window.location.host}/${prefixGithubPages}` + e.target.hash);
-
-        } else {
-            window.location.hash = e.target.hash.replace("#", "")
-        }
-        onRouteChanged()
-    })
+    if (flag) {
+      history.pushState(
+        {
+          id: null
+        },
+        null,
+        `https://${window.location.host}/${prefixGithubPages}` + e.target.hash
+      )
+    } else {
+      window.location.hash = e.target.hash.replace('#', '')
+    }
+    onRouteChanged()
+  })
 })
